b6e8f665d0f4af72fbfb2dd96301460af4a0b345
MauricioTl23 <teranmauricio22@gmail.com>
2025-09-30 20:40:31 -0400

    Quitar app 'main' de INSTALLED_APPS, ajustes para despliegue

--- DeciCASE/urls.py
+++ DeciCASE/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from DeciCASE.views import main
-from . import views
+from DeciCASE.views import main, save_objectives
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main, name='home'),  # raíz del sitio
+    path('', main, name='home'),
     path('main/', main),
-    path('save/', views.save_objectives, name='save_objectives'),
+    path('save/', save_objectives, name='save_objectives'),
 ]
