3845e3ba54199190558d45e8562eedefdd7787c8
MauricioTl23 <teranmauricio22@gmail.com>
2025-09-30 20:48:17 -0400

    Configurar WhiteNoise para servir CSS en producción

--- DeciCASE/settings.py
+++ DeciCASE/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <<<<<<
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'DeciCASE' / 'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- DeciCASE/urls.py
+++ DeciCASE/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from DeciCASE import views 
+from main.views import main, save_objectives  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='home'),  
-    path('main/', views.main),
-    path('save/', views.save_objectives, name='save_objectives'),
+    path('', main, name='home'),
+    path('main/', main),
+    path('save/', save_objectives, name='save_objectives'),
 ]
