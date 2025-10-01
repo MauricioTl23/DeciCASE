902c726a8f2b3f34e9c2be2bacb3c7e6590609aa
MauricioTl23 <teranmauricio22@gmail.com>
2025-09-30 20:30:32 -0400

    Version Final

--- DeciCASE/urls.py
+++ DeciCASE/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for DeciCASE project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from DeciCASE.views import main
@@ -21,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',main),
+    path('', main, name='home'),  # raíz del sitio
+    path('main/', main),
     path('save/', views.save_objectives, name='save_objectives'),
 ]
